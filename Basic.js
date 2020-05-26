1ef756c914b0b39f2254e53e019cd174e05e3d1e
Patrick Miglar <miglar.patrick@gmail.com>
2020-05-26 11:51:38 +0200

    weather

--- Basic.js
+++ Basic.js
@@ -10,12 +10,12 @@ var temp = (Math.random() * (max -min)) +min;
 switch (true) {
    case (temp > -5 && temp < 10):
        title = "The weather is cold";
-       document.write("<figure><img src='cold.jpg' alt='cold'><figcaption>"+title+"</figcaption></figure>");
+       document.write("<figure><img src='img/cold.jpg' alt='cold'><figcaption>"+title+"</figcaption></figure>");
        console.log(title);
        break;
    default:
-       title = "The wather is moderate";
-       document.write("<figure><img src='mod.jpg' alt='moderate'><figcaption>"+title+"</figcaption></figure>");
+       title = "The weather is moderate";
+       document.write("<figure><img src='img/mod.jpg' alt='moderate'><figcaption>"+title+"</figcaption></figure>");
        console.log(title);
        break;
 }

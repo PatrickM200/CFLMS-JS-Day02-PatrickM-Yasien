6f1be35fc670ea662d38903dc675bf558c59c9bd
Patrick Miglar <miglar.patrick@gmail.com>
2020-05-26 11:18:47 +0200

    css js html done

---
+++ Basic.js
@@ -0,0 +1,25 @@
+
+
+
+var min =-5;
+var max =25;
+var temp = (Math.random() * (max -min)) +min;
+
+
+
+switch (true) {
+   case (temp > -5 && temp < 10):
+       title = "The weather is cold";
+       document.write("<figure><img src='cold.jpg' alt='cold'><figcaption>"+title+"</figcaption></figure>");
+       console.log(title);
+       break;
+   default:
+       title = "The wather is moderate";
+       document.write("<figure><img src='mod.jpg' alt='moderate'><figcaption>"+title+"</figcaption></figure>");
+       console.log(title);
+       break;
+}
+console.log(temp);
+
+var number = [1, 7, -3, 9];
+console.log(Math.max(...number));
